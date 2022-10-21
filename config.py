a65c4cbdd42451a9c79fc4eae21fc131c9d70eeb
ValkyFischer <fischer@valkyteq.com>
2022-10-21 09:13:17 +0200

    0.2.0: - Allow ini web lookup

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import configparser
+from urllib.request import urlopen
+
+import requests as requests
+
 
 class Config:
 	def __init__(self, path: str):
@@ -10,8 +14,18 @@ class Config:
 		:param ini: Path to config.ini
 		:return: config as dict
 		"""
-		# Read config
+		# Read config file
 		config = configparser.ConfigParser()
-		config.read(self.ini)
+		if "http" not in self.ini:
+			config.read(self.ini)
+		else:
+			response = requests.get(self.ini)
+			if response.status_code == 200:
+				ini = f"{urlopen(self.ini).read()}"[2:-1].replace("\\n", "\n")
+				config.read_string(ini)
+
+			else:
+				config = False
+
 		# Return
 		return config
